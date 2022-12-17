0610b6387eb750bce6ef3aa85884ea18738672a4
Emanuelle <emanuellemarreira@gmail.com>
2022-12-16 22:55:10 -0400

    Add files via upload

---
+++ 20221215_001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	float num1, num2, resul;
+	printf("digite o primeiro numero= ");
+	scanf("%f",&num1);
+	printf("digite o segundo numero= ");
+	scanf("%f",&num2);
+	resul=num1+num2;
+	printf("resultado= %.2f", resul);
+	return 0;
+}
