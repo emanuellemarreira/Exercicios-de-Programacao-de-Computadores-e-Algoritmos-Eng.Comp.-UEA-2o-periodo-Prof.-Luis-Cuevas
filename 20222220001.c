d3204f45f38f2d56e6ad9f9ed28530d03a8620a5
Emanuelle <emanuellemarreira@gmail.com>
2022-12-20 17:42:56 -0400

    Add files via upload

---
+++ 20222220001.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	float num;
+	float soma=0;
+	
+	scanf("%f", &num);
+	soma = soma + num;
+	scanf("%f", &num);
+	soma = soma + num;
+	scanf("%f", &num);
+	soma = soma + num;
+	scanf("%f", &num);
+	soma = soma + num;
+	scanf("%f", &num);
+	soma = soma + num;
+	printf("\nsoma = %.2f", soma);
+	
+	return 0;
+}
