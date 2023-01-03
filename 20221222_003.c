cbdf51c64ced784b217330cca7799a0d0cc3e85e
Emanuelle <emanuellemarreira@gmail.com>
2023-01-02 21:57:29 -0300

    Update 20221222_003.c

--- 20221222_003.c
+++ 20221222_003.c
@@ -5,16 +5,18 @@ int main()
 {
     int somapares = 0,somarimpares = 0,numero = 0;
     while (numero < 1000){
+        printf("digite o numero = ");
         scanf("%d",&numero);
-        
+        if (numero < 1000){
         if (numero % 2 == 0){
             somapares = somapares + numero;
             }
         if (numero % 2 != 0) {
             somarimpares = somarimpares + numero;
         }
+        }
 
 
     }
-    printf("Soma dos numeros pares: %d \n Soma dos numero impares: %d",somapares,somarimpares);
+    printf("Soma dos numeros pares: %d \nSoma dos numero impares: %d",somapares,somarimpares);
 }
